cbc1b9537a53c3164672b91409c26c22f5b62f74
Kim-hanseul <hansul80@gmail.com>
2022-03-17 18:06:24 +0900

    2022.03.17 18:00

--- titanic/__init__.py
+++ titanic/__init__.py
@@ -1,21 +1,22 @@
-#https://github.com/datasciencedojo/datasets/blob/master/titanic.csv
-from titanic.views import TitanicView
+# https://github.com/datasciencedojo/datasets/blob/master/titanic.csv
+from context.domains import Dataset
 from titanic.models import TitanicModel
-from titanic.templates import TitanicTemplates
+from titanic.templates import TitanicTemplate
+from titanic.views import TitanicView
 if __name__ == '__main__':
     view = TitanicView()
-    model = TitanicModel(train_fname='train.csv',
-                         test_fname='test.csv')
-    template = TitanicTemplates
+    this = Dataset()
 
     while 1:
 
         menu = input('1.템플릿 2.전처리')
         if menu == '1':
-            print(' #### 1. 템플릿 #### ')
-            template = TitanicTemplates(fname='train.csv')
+            print(' #### 1. 템플릿 #### \n')
+            template = TitanicTemplate(fname='train.csv')
+            template.visualize()
             break
         elif menu == '2':
-            print(' #### 2. 전처리 #### ')
-            model = TitanicModel(train_fname='train.csv', test_fname='test.csv')
+            print(' #### 2. 전처리 #### \n')
+            model = TitanicModel()
+            model.preprocess(train_fname='train.csv',test_fname='test.csv')
             break
--- titanic/models.py
+++ titanic/models.py
@@ -3,72 +3,100 @@ from context.models import Model
 from context.domains import Dataset
 
 
+
 class TitanicModel(object):
     model = Model()
     dataset = Dataset()
-    def __init__(self, train_fname, test_fname):
-        self.train = self.model.new_model(train_fname)
-        self.test = self.model.new_model(test_fname)
-        # id 추출
 
-    def preprocess(self):
-        df = self.train
-        ic(f'트레인 컬럼 {self.df.columns}')
-        ic(f'트레인 헤드 {self.df.head()}')
-        ic(df)
-        df = self.drop_feature(df)
-        df = self.create_label(df)
-        df = self.create_train(df)
-        df = self.embarked_nominal(df)
-        df = self.pclass_ordinal(df)
-        df = self.name_nominal(df)
-        df = self.sex_nominal(df)
-        df = self.age_ratio(df)
-        df = self.fare_ratio(df)
-        return df
-
-    @staticmethod
-    def create_label(df)->object:
-        return TitanicModel.create_train(df)
-
-    @staticmethod
-    def create_train(df)->object:
-        return TitanicModel.drop_feature(df)
-
-    def drop_feature(self,df)->object:
-        for i in []:
-            ()
+    def preprocess(self, train_fname, test_fname):
+        this = self.dataset
+        that = self.model
+        this.train = that.new_dframe(train_fname)
+        this.test = that.new_dframe(test_fname)
+        this.id = this.test['PassengerId']
+        this.label = this.train['Surivived']
+        this.train = this.train.drop('Survived', axis=1)
+        this.test = this.test.drop('Survived', axis=1)
+        # Entity 에서 Object 로 변환
+        this = self.drop_feature(this)
         '''
+        this = self.create_train(this)
+        this = self.embarked_nominal(this)
+        this = self.pclass_ordinal(this)
+        this = self.name_nominal(this)
+        this = self.sex_nominal(this)
+        this = self.age_ratio(this)
+        this = self.fare_ratio(this)
+        '''
+        self.print_this(this)
+        return this
+
+    @staticmethod
+    def print_this(this):
+        ic(f'1. train 의 type : {type(this.train)}\n')
+        ic(f'2. train 의 column : {this.train.columns}\n')
+        ic(f'3. train 의 상위 1개 : {this.train.head(1)}\n')
+        ic(f'4. train 의 null의 개수 : {this.train.isnull().sum()}\n')
+        ic(f'5. test 의 type : {type(this.test)}\n')
+        ic(f'6. test 의 column : {this.test.columns}\n')
+        ic(f'7. test 의 상위 1개 : {this.test.head(1)}\n')
+        ic(f'8. train 의 null의 개수 : {this.test.isnull().sum()}\n')
+        ic(f'9. id 의 타입 : {type(this.id)}\n')
+        ic(f'10. id 의 상위 10개 : {this.id[:10]}\n')
+
+
+    def create_this(self, dataset)->object:
+        this = dataset
+        this.train = self.train
+        this.test = self.test
+        this.id = self.id
+        return this
+
+
+    @staticmethod
+    def create_train(this) -> object:
+        return this
+    @staticmethod
+    def drop_feature(this, *feature) -> object:
+        feature = ['Cabin', 'Parch', 'Ticket', 'SibSp']
+        this.train = [this.train.drop(i, axis=1) for i in feature]
+        this.test = [this.test.drop(i, axis=1) for i in feature]
+        [this.drop([*feature], axis=1, inplace=True) for i in [*feature]]
+
+        '''
+        self.cabin_garbage(df)
+        self.parch_garbage(df)  
         self.ticket_garbage(df)
         self.sib_sp_garbage(df)
-        self.cabin_garbage(df)
-        self.parch_garbage(df)
         '''
-        return df
+        return this
 
+    '''
+    Categorical vs. Quantitative
+    Cate -> nominal (이름) vs. ordinal (순서)
+    Quan -> interval (상대) vs. ratio (절대)
+    '''
 
-    # Categorical vs Quantitative
-    # Cate -> nominal (이름) vs. ordinal (순서)
-    # Quan -> interval (상대) vs. ratio (절대)
-
     @staticmethod
-    def pclass_ordinal(df)->object:
-        return df
-    @staticmethod
-    def name_nominal(df)->object: # 네임에서 계급이 떼어내야함
-        return df
-    @staticmethod
-    def sex_nominal(df)->object:
-        return df
-    @staticmethod
-    def age_ratio(df)->object:
+    def pclass_ordinal(df) -> object:
         return df
 
     @staticmethod
-    def fare_ratio(df)->object:
+    def name_nominal(df) -> object:
         return df
 
     @staticmethod
-    def embarked_nominal(df)->object: #항구에 따라 소득수준이 다름
+    def age_ratio(df) -> object:
         return df
 
+    @staticmethod
+    def sex_nominal(df) -> object:
+        return df
+
+    @staticmethod
+    def embarked_nominal(df) -> object:
+        return df
+
+    @staticmethod
+    def fare_ratio(df) -> object:
+        return df
